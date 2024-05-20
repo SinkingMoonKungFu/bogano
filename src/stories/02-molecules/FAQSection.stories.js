8247eae18fa6c5a962c513959aee5d4ea09cb55f
Scott Johnson <jaywir3@gmail.com>
2024-05-20 11:39:34 -0500

    :white_check_mark: Fix FAQSection component within storybook.
    This fixes the story in storybook and adds chromatic to the package.json
    file.

--- src/stories/02-molecules/FAQSection.stories.js
+++ src/stories/02-molecules/FAQSection.stories.js
@@ -12,6 +12,15 @@ export default {
 
 export const Default = {
   args: {
-    title: "New Students"
+    title: "New Students",
+    questions: [
+        {
+            question: "What should I wear to my first class?",
+            answer: [
+                "Loose-fitting clothing is preferable. Sweatpants and a T-shirt is generally fine, along with a pair of low-tread shoes (wrestling shoes, martial arts shoes, etc.). In order to maintain cleanliness and avoid damage to our equipment, street shoes, especially sneakers, are not allowed on the mats during workouts. If you don't have a pair of low-tread shoes, you may remove your shoes and socks and workout barefoot.",
+                "Avoid clothing that is too constricting, such as button-up shirts and dress pants. It's also good to avoid wearing jeans, if possible, since they don't give the flexibility necessary to perform some of our kicks (as well as being abrasive and uncomfortable)."
+            ]
+        }
+    ]
   }
 };
